3ccc6290cd37563f30b57423c18fe2d60b036c51
alhabib911 <abdullahalhabib100@gmail.com>
2022-05-16 17:54:23 +0600

    date picker added

--- src/Home/Booking.js
+++ src/Home/Booking.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import bg from '../image/background/bg.png'
 import './Booking.css'
+import DatePicker from "react-multi-date-picker"
+import DatePanel from "react-multi-date-picker/plugins/date_panel"
 import { AiFillCalendar } from 'react-icons/ai';
 
-
 const Booking = () => {
+
     return (
         <div>
             <div className='bg'
@@ -21,17 +23,26 @@ const Booking = () => {
                     </div>
                     <div className='booking-form'>
                         <form>
+                            <div class="form-control">
+                                <select class="select select-ghost w-full max-w-xs">
+                                    <option disabled selected>Trip Catagory</option>
+                                    <option>One Way</option>
+                                    <option>Round Way</option>
+                                </select>
+                            </div>
                             <div className="booking-form-field">
                                 <div className="location gap-2">
                                     <div className="picup-location">
                                         <h2>FROM</h2>
                                         <select class="select-bordered">
                                             <option disabled selected>Select a city</option>
-                                            <option>Star Wars</option>
-                                            <option>Harry Potter</option>
-                                            <option>Lord of the Rings</option>
-                                            <option>Planet of the Apes</option>
-                                            <option>Star Trek</option>
+                                            <option>Dhaka</option>
+                                            <option>Barisal</option>
+                                            <option>Chittagong</option>
+                                            <option>Khulna </option>
+                                            <option>Mymensingh </option>
+                                            <option>Rajshahi  </option>
+                                            <option>Sylhet  </option>
                                         </select>
                                         <p>Click to choose your Location</p>
                                     </div>
@@ -39,11 +50,13 @@ const Booking = () => {
                                         <h2>TO</h2>
                                         <select class="select-bordered">
                                             <option disabled selected>Select a city</option>
-                                            <option>Star Wars</option>
-                                            <option>Harry Potter</option>
-                                            <option>Lord of the Rings</option>
-                                            <option>Planet of the Apes</option>
-                                            <option>Star Trek</option>
+                                            <option>Dhaka</option>
+                                            <option>Barisal</option>
+                                            <option>Chittagong</option>
+                                            <option>Khulna </option>
+                                            <option>Mymensingh </option>
+                                            <option>Rajshahi  </option>
+                                            <option>Sylhet  </option>
                                         </select>
                                         <p>Click to choose your Location</p>
                                     </div>
@@ -52,27 +65,28 @@ const Booking = () => {
                                     <div className='date'>
                                         <div className="journey-date">
                                             <h2>Journey Date</h2>
-                                            <div className='calender'>
-                                                <input type="text" name="journey-date" id="" placeholder='Journey Date' />
-                                                <div className="calender-icon">
-                                                    <AiFillCalendar></AiFillCalendar>
+                                            <div className="calender-field">
+                                                <div className="calender">
+                                                    <DatePicker placeholder='Select Date'
+                                                        inputClass="custom-input"
+                                                    />
                                                 </div>
+                                                <p>Click to choose your Journey Date</p>
                                             </div>
                                         </div>
                                         <div className="return-date">
                                             <h2>Return Date</h2>
-                                            <div className="calender">
-                                                <input type="text" name="return-date" id="" placeholder='Return-Date' />
-                                                <div className="calender-icon">
-                                                    <AiFillCalendar></AiFillCalendar>
+                                            <div className="calender-field">
+                                                <div className="calender">
+                                                    <DatePicker placeholder='Select Date'
+                                                        inputClass="custom-input"
+                                                    />
                                                 </div>
+                                                <p>Click to choose your Journey Date</p>
                                             </div>
                                         </div>
-
                                     </div>
-
                                     <div className='time'>
-
                                     </div>
                                 </div>
                             </div>
@@ -82,8 +96,8 @@ const Booking = () => {
                         </form>
                     </div>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 };
 
